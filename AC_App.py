5662b7839778572c4bc22e7fc6286b2a51327455
TacoJimmy <t5669016@ntut.org.tw>
2023-04-23 14:58:53 +0800

    Add files via upload

---
+++ AC_App.py
@@ -0,0 +1,167 @@
+# coding:utf-8
+import codecs
+import json
+import ssl
+import paho.mqtt.client as mqtt
+import time
+import AC_Ctrl
+import threading
+import time
+import schedule  
+
+
+
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code"+str(rc))
+    client.subscribe('v1/devices/me/rpc/request/+',1)
+    time.sleep(3)
+
+def ipcaccontrl(data_payload):
+    print (data_payload)
+    print (data_payload['params'])
+    
+    
+    if data_payload['method'] == 'AC1_SetOnOff':
+        if data_payload['params'] == False:
+            AC_Ctrl.AC_TrunONOFF('/dev/ttyS4', 123, 0)
+            
+        
+        if data_payload['params'] == True:
+            AC_Ctrl.AC_TrunONOFF('/dev/ttyS4', 123, 1)
+    if data_payload['method'] == 'AC1_SetTemp':
+        AC_Ctrl.AC_TrunTemp('/dev/ttyS4', 123, data_payload['params'])
+        
+    if data_payload['method'] == 'AC2_SetOnOff':
+        if data_payload['params'] == False:
+            AC_Ctrl.AC_TrunONOFF('/dev/ttyS4', 155, 0)
+        
+        if data_payload['params'] == True:
+            AC_Ctrl.AC_TrunONOFF('/dev/ttyS4', 155, 1)
+    if data_payload['method'] == 'AC2_SetTemp':
+        AC_Ctrl.AC_TrunTemp('/dev/ttyS4', 155, data_payload['params'])
+        
+    if data_payload['method'] == 'AC3_SetOnOff':
+        if data_payload['params'] == False:
+            AC_Ctrl.AC_TrunONOFF('/dev/ttyS4', 156, 0)
+        
+        if data_payload['params'] == True:
+            AC_Ctrl.AC_TrunONOFF('/dev/ttyS4', 156, 1)
+    if data_payload['method'] == 'AC3_SetTemp':
+        AC_Ctrl.AC_TrunTemp('/dev/ttyS4', 156, data_payload['params'])
+        
+    if data_payload['method'] == 'AC4_SetOnOff':
+        if data_payload['params'] == False:
+            AC_Ctrl.AC_TrunONOFF('/dev/ttyS4', 157, 0)
+        
+        if data_payload['params'] == True:
+            AC_Ctrl.AC_TrunONOFF('/dev/ttyS4', 157, 1)
+    if data_payload['method'] == 'AC4_SetTemp':
+        AC_Ctrl.AC_TrunTemp('/dev/ttyS4', 157, data_payload['params'])
+            
+            
+def on_message(client, userdata, msg):
+    data_topic = msg.topic
+    data_payload = json.loads(msg.payload.decode())
+    time.sleep(.5)
+    listtopic = data_topic.split("/")  
+    signal_fb = 'v1/devices/me/rpc/response/'+str(listtopic[5]) # response topic
+    
+    fb_payload = ipcaccontrl(data_payload)
+    
+    client.publish(signal_fb,json.dumps(fb_payload)) # send response
+
+
+def ipc_subscribe():
+    meter_token = 'fjfp5i9VxcPRC7FrChr0'
+    meter_pass = ''
+    url = 'thingsboard.cloud'
+
+    client = mqtt.Client('', True, None)
+    client.on_connect = on_connect
+    time.sleep(.5)
+    client.on_message = on_message
+    time.sleep(.5)
+    client.username_pw_set(meter_token, meter_pass)
+    client.connect(url, 1883, 60)
+
+
+    client.loop_forever()
+
+def AC_Infor():
+    meter_token = 'k6XyHLptVPFxXgrT7tm5'
+    meter_pass = ''
+    url = 'thingsboard.cloud'
+
+    client1 = mqtt.Client('', True, None)
+    client1.username_pw_set(meter_token, meter_pass)
+    client1.connect(url, 1883, 60)
+    time.sleep(1)
+    
+    #AC1
+    AC1_Infor = AC_Ctrl.AC_ReadFullFunction('/dev/ttyS4',123)
+    if AC1_Infor[5] == 2:
+        AC1_Infor = AC_Ctrl.AC_ReadFullFunction('/dev/ttyS4',123)
+    payload = {'AC1_Status' : AC1_Infor[0],
+               'AC1_mode':AC1_Infor[1],
+               'AC1_windspeed':AC1_Infor[2],
+               'AC1_settemp':AC1_Infor[3],
+               'AC1_roomtemp':AC1_Infor[4],
+               'AC1_linkinfor':AC1_Infor[5] }
+    print (json.dumps(payload))
+    if AC1_Infor[5] != 2:
+        client1.publish('v1/devices/me/telemetry', json.dumps(payload))
+        time.sleep(5)
+    #AC2
+    AC2_Infor = AC_Ctrl.AC_ReadFullFunction('/dev/ttyS4',155)
+    if AC2_Infor[5] == 2:
+        AC2_Infor = AC_Ctrl.AC_ReadFullFunction('/dev/ttyS4',155)
+    payload = {'AC2_Status' : AC2_Infor[0],
+               'AC2_mode':AC2_Infor[1],
+               'AC2_windspeed':AC2_Infor[2],
+               'AC2_settemp':AC2_Infor[3],
+               'AC2_roomtemp':AC2_Infor[4],
+               'AC2_linkinfor':AC2_Infor[5] }
+    print (json.dumps(payload))
+    if AC2_Infor[5] != 2:
+        client1.publish('v1/devices/me/telemetry', json.dumps(payload))
+        time.sleep(5)
+    #AC3
+    AC3_Infor = AC_Ctrl.AC_ReadFullFunction('/dev/ttyS4',156)
+    if AC3_Infor[5] == 2:
+        AC3_Infor = AC_Ctrl.AC_ReadFullFunction('/dev/ttyS4',156)
+    payload = {'AC3_Status' : AC3_Infor[0],
+               'AC3_mode':AC3_Infor[1],
+               'AC3_windspeed':AC3_Infor[2],
+               'AC3_settemp':AC3_Infor[3],
+               'AC3_roomtemp':AC3_Infor[4],
+               'AC3_linkinfor':AC3_Infor[5] }
+    print (json.dumps(payload))
+    if AC3_Infor[5] != 2:
+        client1.publish('v1/devices/me/telemetry', json.dumps(payload))
+        time.sleep(5)
+    #AC4
+    AC4_Infor = AC_Ctrl.AC_ReadFullFunction('/dev/ttyS4',157)
+    if AC4_Infor[5] == 2:
+        AC4_Infor = AC_Ctrl.AC_ReadFullFunction('/dev/ttyS4',157)
+    payload = {'AC4_Status' : AC4_Infor[0],
+               'AC4_mode':AC4_Infor[1],
+               'AC4_windspeed':AC4_Infor[2],
+               'AC4_settemp':AC4_Infor[3],
+               'AC4_roomtemp':AC4_Infor[4],
+               'AC4_linkinfor':AC4_Infor[5] }
+    print (json.dumps(payload))
+    if AC4_Infor[5] != 2:
+        client1.publish('v1/devices/me/telemetry', json.dumps(payload))
+        time.sleep(5)
+
+schedule.every(1).minutes.do(AC_Infor)
+ 
+t = threading.Thread(target=ipc_subscribe)
+t.start()
+    
+    
+if __name__ == '__main__':
+    while True:  
+        schedule.run_pending()  
+        time.sleep(1)  
+    
---
+++ AC_Ctrl.py
@@ -0,0 +1,224 @@
+import time
+import serial
+import modbus_tk.defines as cst
+from modbus_tk import modbus_rtu
+
+
+def AC_PowerONOFF(PORT,ID,mode):
+    try:
+        master = modbus_rtu.RtuMaster(serial.Serial(port=PORT, baudrate=9600, bytesize=8, parity='N', stopbits=1, xonxoff=0))
+        master.set_timeout(5.0)
+        master.set_verbose(True)
+        AC_status = master.execute(ID, cst.READ_HOLDING_REGISTERS, 0, 1)
+        time.sleep(1)
+        i = 0
+        while AC_status[0] != mode and i < 5:
+            master.execute(ID, cst.WRITE_SINGLE_REGISTER, 0, output_value=mode)
+            time.sleep(1)
+            AC_status = master.execute(ID, cst.READ_HOLDING_REGISTERS, 0, 1)
+            time.sleep(1)
+            i = i + 1
+        return (AC_status[0])
+        
+    except:
+        return ('loss_connect')
+    
+     
+    
+def AC_OPset(PORT,ID,mode): # Operaction  mode = (0=AC,1=humidi, 2=fan only) 
+    try:
+        master = modbus_rtu.RtuMaster(serial.Serial(port=PORT, baudrate=9600, bytesize=8, parity='N', stopbits=1, xonxoff=0))
+        master.set_timeout(5.0)
+        master.set_verbose(True)
+        AC_OP = master.execute(ID, cst.READ_HOLDING_REGISTERS, 1, 1) 
+        time.sleep(0.5)
+        i = 0
+        while AC_OP[0] != mode and i < 5:
+            AC_OPValue = master.execute(ID, cst.WRITE_SINGLE_REGISTER, 1, output_value=mode)
+            time.sleep(1)
+            AC_OP = master.execute(ID, cst.READ_HOLDING_REGISTERS, 1, 1)
+            i = i + 1
+        master.close()
+        return (AC_OP[0])
+        
+    except:
+        master.close()
+        return ('loss_connect')
+
+def AC_FanSpeed(PORT,ID,mode): # mode = (0=auto,1~15 15=highest) 
+    try:
+        
+        master = modbus_rtu.RtuMaster(serial.Serial(port=PORT, baudrate=9600, bytesize=8, parity='N', stopbits=1, xonxoff=0))
+        master.set_timeout(5.0)
+        master.set_verbose(True)
+        AC_Fan = master.execute(ID, cst.READ_HOLDING_REGISTERS, 2, 1)
+        time.sleep(0.5)
+        i = 0
+        if mode >=0 and mode<=15:
+            while AC_Fan[0] != mode and i < 5:
+                AC_FanValue = master.execute(ID, cst.WRITE_SINGLE_REGISTER, 2, output_value=mode)
+                time.sleep(1)
+                AC_Fan = master.execute(ID, cst.READ_HOLDING_REGISTERS, 2, 1)
+                i = i + 1
+        master.close()
+        return (AC_Fan[0])
+        
+    except:
+        master.close()
+        return ('loss_connect')
+
+def AC_SetTemp(PORT,ID,mode): # mode = temp
+    try:
+        
+        master = modbus_rtu.RtuMaster(serial.Serial(port=PORT, baudrate=9600, bytesize=8, parity='N', stopbits=1, xonxoff=0))
+        master.set_timeout(5.0)
+        master.set_verbose(True)
+        AC_settemp = master.execute(ID, cst.READ_HOLDING_REGISTERS, 3, 1)
+        time.sleep(0.5)
+        i = 0
+        if mode <=30 and mode>=18:
+            while AC_settemp[0] != mode and i < 5 :
+                AC_settempvalue = master.execute(ID, cst.WRITE_SINGLE_REGISTER, 3, output_value=mode)
+                time.sleep(1)
+                AC_settemp = master.execute(ID, cst.READ_HOLDING_REGISTERS, 3, 1)
+                i = i + 1
+        master.close()
+        return (AC_settemp[0])
+        
+    except:
+        master.close()
+        return ('loss_connect')
+
+def AC_ReadHumi(PORT,ID):
+    try:
+        master = modbus_rtu.RtuMaster(serial.Serial(port=PORT, baudrate=9600, bytesize=8, parity='N', stopbits=1, xonxoff=0))
+        master.set_timeout(5.0)
+        master.set_verbose(True)
+        AC_readhumid = master.execute(ID, cst.READ_HOLDING_REGISTERS, 1094, 1)
+        AC_humid = AC_readhumid[0] * 0.01
+        return (AC_humid)
+    except:
+        master.close()
+        return ('loss_connect')
+
+def AC_ReadOPTemp(PORT,ID): 
+    try:
+        
+        master = modbus_rtu.RtuMaster(serial.Serial(port=PORT, baudrate=9600, bytesize=8, parity='N', stopbits=1, xonxoff=0))
+        master.set_timeout(5.0)
+        master.set_verbose(True)
+        AC_readtemp = master.execute(ID, cst.READ_HOLDING_REGISTERS, 4, 1)
+        time.sleep(0.5)
+        
+        master.close()
+        return (AC_readtemp[0])
+        
+    except:
+        master.close()
+        return ('loss_connect')    
+    
+def AC_ReadSetTemp(PORT,ID): 
+    try:
+        
+        master = modbus_rtu.RtuMaster(serial.Serial(port=PORT, baudrate=9600, bytesize=8, parity='N', stopbits=1, xonxoff=0))
+        master.set_timeout(5.0)
+        master.set_verbose(True)
+        AC_readtemp = master.execute(ID, cst.READ_HOLDING_REGISTERS, 3, 1)
+        time.sleep(0.5)
+        master.close()
+        return (AC_readtemp[0])
+        
+    except:
+        master.close()
+        return ('loss_connect')
+
+
+def AC_ReadFullFunction(PORT,ID): # (value 0=on/off, 1=op mode, 2=fan speed, 3=set temp, 4=read temp)
+    try:
+        AC_infor = [0,0,0,0,0,0]
+        if ID != 6:
+            master = modbus_rtu.RtuMaster(serial.Serial(port=PORT, baudrate=9600, bytesize=8, parity='N', stopbits=1, xonxoff=0))
+            master.set_timeout(5.0)
+            master.set_verbose(True)
+            AC_FullFunction = master.execute(ID, cst.READ_HOLDING_REGISTERS, 0, 5)
+            time.sleep(0.2)
+            AC_error = master.execute(ID, cst.READ_HOLDING_REGISTERS, 41, 1)
+            time.sleep(0.2)
+            AC_errorall = AC_error[0]
+            AC_infor[0] = AC_FullFunction[0]
+            AC_infor[1] = AC_FullFunction[1]
+            AC_infor[2] = AC_FullFunction[2]
+            AC_infor[3] = AC_FullFunction[3]
+            AC_infor[4] = AC_FullFunction[4]
+            AC_infor[5] = 1
+            if AC_errorall == 1:
+                AC_infor[5] = 0
+        
+            master.close()
+        else:
+            AC_infor = [0,0,0,0,0,3]
+        return (AC_infor)
+    except:
+        AC_infor[0] = 0
+        AC_infor[1] = 0
+        AC_infor[2] = 0
+        AC_infor[3] = 0
+        AC_infor[4] = 0
+        AC_infor[5] = 2
+        master.close()
+        return (AC_infor)
+
+def AC_error(PORT,ID): # (value 0=on/off, 1=op mode, 2=fan speed, 3=set temp, 4=read temp)
+    AC_infor = [0,0,0,0,0,0]
+    
+    master = modbus_rtu.RtuMaster(serial.Serial(port=PORT, baudrate=9600, bytesize=8, parity='N', stopbits=1, xonxoff=0))
+    master.set_timeout(5.0)
+    master.set_verbose(True)
+            
+            
+    AC_error = master.execute(ID, cst.READ_HOLDING_REGISTERS, 514, 1)
+    return AC_error        
+
+def AC_TrunONOFF(PORT, ID, mode):
+    AC_CtrlStatus = AC_PowerONOFF(PORT,ID,mode)
+    if AC_CtrlStatus == 'loss_connect' :
+        i = 0
+        while (AC_CtrlStatus == "loss_connect") and (i < 5) :
+            AC_CtrlStatus = AC_PowerONOFF(PORT,ID,mode)
+            i = i + 1
+            time.sleep(.5)
+    return ('done')
+
+def AC_TrunMode(PORT, ID, mode):
+    AC_CtrlStatus = AC_OPset(PORT,ID,mode)
+    if AC_CtrlStatus == 'loss_connect' :
+        i = 0
+        while (AC_CtrlStatus == "loss_connect") and (i < 5) :
+            AC_CtrlStatus = AC_OPset(PORT,ID,mode)
+            i = i + 1
+            time.sleep(.5)
+    return ('done')
+
+def AC_TrunTemp(PORT, ID, mode):
+    AC_CtrlStatus = AC_SetTemp(PORT,ID,mode)
+    if AC_CtrlStatus == 'loss_connect' :
+        i = 0
+        while (AC_CtrlStatus == "loss_connect") and (i < 5) :
+            AC_CtrlStatus = AC_SetTemp(PORT,ID,mode)
+            i = i + 1
+            time.sleep(.5)
+    return ('done')
+
+if __name__ == '__main__':
+    while True:
+        #print (AC_TrunONOFF('/dev/ttyS4', 123, 0))
+        '''
+        print (AC_TrunONOFF('/dev/ttyS4', 123, 1))
+        print (AC_TrunMode('/dev/ttyS4', 123, 0))
+        print (AC_TrunTemp('/dev/ttyS4', 123, 26))
+        '''
+        
+        print (AC_ReadFullFunction('/dev/ttyS4',123))
+        
+        
+        time.sleep(5)
